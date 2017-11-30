8f51e9c7f5afb31bafa36352c8e579eae30ef57d
Blaise <imbasheep@gmail.com>
2017-11-30 17:15:40 +0100

    local url

--- src/actions/index.js
+++ src/actions/index.js
@@ -47,9 +47,9 @@ export const EXPAND_CHAT = 'expand_chat';
 export const HIDE_CHAT = 'hide_chat';
 export const PRIVATE_CHAT = 'private_chat';
 
-// const ROOT_URL = 'https://safe-badlands-67690.herokuapp.com';
+const ROOT_URL = 'https://safe-badlands-67690.herokuapp.com';
 
-const ROOT_URL = 'http://localhost:3007';
+// const ROOT_URL = 'http://localhost:3007';
 
 export function addPrivate(room, message){
 
